4bbb507a917061479e883c279a6be31429b940f9
unknown <Edusan17>
2022-09-28 12:50:19 -0300

    Primeiros passos CSS puro

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://wallpapercave.com/wp/wp9376449.jpg" className='img'/>
+        </>
+    )
+
+}
+
+export default Home;
