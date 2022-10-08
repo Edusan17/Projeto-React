d1b2bde564c2bab1ee5caa235876be28e8a41eaf
Edusan17 <santos.eduuh567@gmail.com>
2022-10-08 01:41:03 -0300

    Tema,Postagem e atualizando Navbar

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -3,10 +3,20 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Box, Grid, Button } from '@mui/material';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 import './Navbar.css'
 
 function Navbar() {
+    const [token, setToken] = useLocalStorage('token');
+    let navigate = useNavigate();
+
+    function goLogout() {
+        setToken('')
+        alert('Usuário deslogado!')
+        navigate('/login')
+    }
+
     return (
         <>
             <AppBar position="static" style={{ backgroundColor: 'black' }}>
@@ -18,32 +28,38 @@ function Navbar() {
                     </Box>
                     <Grid container justifyContent="flex-end">
                         <Box display="flex" justifyContent="start">
+                        <Link to='/home' className='text-decorator-none textos'>
                         <Box mx={1} style={{ cursor: "pointer" }} >
                             <Typography variant="h5" color="inherit" className='txt7'>
                                 Home
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to='/posts' className='text-decorator-none textos'>
                         <Box mx={1} style={{ cursor: "pointer" }} >
                             <Typography variant="h5" color="inherit" className="txt7">
                                 Historias
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to='/temas' className='text-decorator-none textos'>
                         <Box mx={1} style={{ cursor: "pointer" }} >
                             <Typography variant="h5" color="inherit" className="txt7">
                                 Personagens
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to='formularioTema' className='text-decorator-none textos'>
                         <Box mx={1} style={{ cursor: "pointer" }} >
                             <Typography variant="h5" color="inherit" className="txt7">
                                 Jogos
                             </Typography>
                         </Box>
-                        <Box mx={1} className='cursor'>
-                            <Link to='/login' className='text-decorator-none'>
+                        </Link>
+                        <Box mx={1} className='cursor' onClick={goLogout}>
                                 <Typography variant="h6" color="inherit" >
                                     <Button className='btn-login txt7' variant='contained'>logout</Button>
                                 </Typography>
-                            </Link>
                         </Box>
                     </Box>
                     </Grid>
--- src/componentes/postagens/listapostagem/ListaPostagem.tsx
+++ src/componentes/postagens/listapostagem/ListaPostagem.tsx
@@ -1,40 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-import { Box } from "@mui/material";
+import { Box } from '@mui/material';
 import './ListaPostagem.css';
+import Postagem from '../../../models/Postagem';
+import useLocalStorage from 'react-use-localstorage';
+import { busca } from '../../../services/Service';
+
 
 function ListaPostagem() {
+  let navigate = useNavigate();
+  const [posts, setPosts] = useState<Postagem[]>([])
+  const [token, setToken] = useLocalStorage('token');
+
+  useEffect(() => {
+    if (token == '') {
+      alert('Realize o Login para ter acesso a função Temas!')
+      navigate('/login')
+    }
+  }, [token])
+
+  async function getPostagem(){
+    await busca('/postagens', setPosts, {
+      headers: {'Authorization': token}
+    })
+  }
+
+  useEffect(() => {
+    getPostagem()
+  }, [posts.length])
 
   return (
     <>
-      <Box m={2} >
-        <Card variant="outlined">
+    {
+      posts.map(post => (
+      <Box m={2} className='card'>
+        <Card variant="outlined" className="card">
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Postagens
             </Typography>
             <Typography variant="h5" component="h2">
-              Título
+              {post.titulo}
             </Typography>
             <Typography variant="body2" component="p">
-              Texto da Postagem
+              {post.texto}
             </Typography>
             <Typography variant="body2" component="p">
-              Tema
+              {post.tema?.descricao}
             </Typography>
           </CardContent>
           <CardActions>
             <Box display="flex" justifyContent="center" mb={1.5}>
-
-              <Link to="" className="text-decorator-none" >
+              <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
                 <Box mx={1}>
                   <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
                   </Button>
                 </Box>
               </Link>
-              <Link to="" className="text-decorator-none">
+              <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" size='small' color="secondary">
                     deletar
@@ -45,6 +70,8 @@ function ListaPostagem() {
           </CardActions>
         </Card>
       </Box>
+      ))
+    }
     </>)
 }
 
