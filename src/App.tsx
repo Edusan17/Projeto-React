740c6f7a659219798678fe10299334eb0dc25d49
Edusan17 <santos.eduuh567@gmail.com>
2022-10-11 14:57:45 -0300

    Ajustes do alerts com React-tostify

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,15 @@ import CadastroPost from "./componentes/postagens/cadastroPost/CadastroPost";
 import DeletarPostagem from "./componentes/postagens/deletarPostagem/DeletarPostagem";
 import CadastroTema from "./componentes/temas/cadastroTema/CadastroTema";
 import DeletarTema from "./componentes/temas/deletarTema/DeletarTema";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
 function App() {
   return (
     <Provider store={store}>
+      <ToastContainer />
     <BrowserRouter>
       <Navbar />
       <div style={{ minHeight: "100vh" }}>
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -7,6 +7,7 @@ import UserLogin from "../../models/UserLogin";
 import { Box } from "@mui/material";
 import { useDispatch } from 'react-redux';
 import { addToken } from "../../store/tokens/actions";
+import { toast } from "react-toastify";
 
 function Login() {
   let history = useNavigate();
@@ -44,9 +45,27 @@ function Login() {
     try{
       await login('/usuarios/logar', userLogin, setToken)
 
-      alert('Usuário logado com sucesso!!!');
+      toast.success('Usuário logado com sucesso', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+    });
     }catch(error){
-      alert('Dados do Usuário inconsistentes. Erro ao logar!!!')
+      toast.error('Dados do usuário inconsistentes. Erro ao logar!', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+    });
     }
   }
   return (
